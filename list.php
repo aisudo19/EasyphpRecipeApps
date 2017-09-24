66c0bc8b8d03d3f0b62dee08d5c1625b16324b02
kaitouEx <indigonaka@gmail.com>
2017-09-25 05:56:09 +0900

    list.phpを追加

---
+++ list.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" />
+	<title>出力結果</title>
+</head>
+<body>
+	<?php
+		$user = "nakamura";
+		$pass = "nakamura";
+        $dsn = "mysql:dbname=db1;host=localhost;port=8889;charset=utf8";
+        // charset=utf8"
+        try{
+            $dbh = new PDO($dsn, $user, $pass);        
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql="SELECT * FROM recipes";
+            $stmt = $dbh->query($sql);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            echo "<table>\n";
+            echo "<tr>\n";
+            echo "<th>料理名</th><th>予算</th><th>難易度</th>\n";
+            foreach($result as $row) {
+                echo "<tr>\n";
+                echo "<td>" . htmlspecialchars($row['recipe_name'], ENT_QUOTES, 'UTF-8') . "</td>\n";
+                echo "<td>" . htmlspecialchars$row['budget'] . "</td>\n";
+                echo "<td>" . $row['difficulty'] . "</td>\n";
+                echo "</tr>\n";            
+            }
+            echo "</tr>\n";            
+            echo "</table>\n";    
+            
+            print_r($result);
+            $dbh = null;
+        }
+        catch(Exception $e){
+            echo 'Connection failed: ' . htmlspecialchars($e->getMessage(),ENT_QUOTES, "UTF-8");
+            die();
+        }
+		    ?>
+</body>
+</html> 
